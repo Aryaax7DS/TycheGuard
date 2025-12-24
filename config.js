52a609e65809e2141271e29639f0e631d604670e
Leviathan. <73642239+Aryaax7DS@users.noreply.github.com>
2025-12-24 18:34:29 +0700

    Update role and channel IDs in config.js

--- config.js
+++ config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  VERIFIED_ROLE_ID: "1450848971562942638",
-  UNVERIFIED_ROLE_ID: "1450849016853172378",
-  VERIFY_CHANNEL_ID: "1450853030168887489",
-  VERIFY_LOG_CHANNEL_ID: "1450864785381130372",
-  SELF_ROLE_CHANNEL_ID: "1451048357542760582",
+  VERIFIED_ROLE_ID: "1453340529587650613",
+  UNVERIFIED_ROLE_ID: "1453340691207028758",
+  VERIFY_CHANNEL_ID: "1453346196591476786",
+  VERIFY_LOG_CHANNEL_ID: "1453349429376716891",
+  SELF_ROLE_CHANNEL_ID: "1453346404041756884",
 
   MIN_ACCOUNT_AGE_DAYS: 7,
 
   SELF_ROLES: {
     ml: {
-      roleId: "1450850399371395172",
+      roleId: "1453340810870521896",
       label: "🎮 Mobile Legends",
       emoji: "🎮"
     },
     roblox: {
-      roleId: "1450850455180541962",
+      roleId: "1453341092639539324",
       label: "🟦 Roblox",
       emoji: "🟦"
     },
     fps: {
-      roleId: "1450850479394521168",
+      roleId: "1453341314128285726",
       label: "🔫 FPS Gamer",
       emoji: "🔫"
     },
     chill: {
-      roleId: "1451075526650368093",
+      roleId: "1453341440154407013",
       label: "🎧 Chill / Nongkrong",
       emoji: "🎧"
     }
